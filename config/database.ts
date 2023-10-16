49bb93e552af1ea04cc3142e9f14f93502d7fdd9
Rafael <jiyuunotameni@hotmail.com>
2023-10-16 08:46:37 -0300

    continue

--- config/database.ts
+++ config/database.ts
@@ -41,6 +41,7 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: Env.get('HAS_SSL'),
       },
       migrations: {
         naturalSort: true,
